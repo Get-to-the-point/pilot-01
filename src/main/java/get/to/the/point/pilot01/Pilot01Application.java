4361937b4f9089bea3d1dd49bf3f1f7372f94485
s_cy <tjcodud6525@naver.com>
2024-12-04 16:58:27 +0900

    init

---
+++ src/main/java/get/to/the/point/pilot01/Pilot01Application.java
@@ -0,0 +1,13 @@
+package get.to.the.point.pilot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pilot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pilot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/get/to/the/point/pilot01/Pilot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package get.to.the.point.pilot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pilot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
