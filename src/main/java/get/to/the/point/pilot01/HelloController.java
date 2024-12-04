1b12f40b6e8062827d033a68585f8ce8060b026d
Minwoo <cap019@naver.com>
2024-12-04 17:09:41 +0900

    hello Controller!

---
+++ src/main/java/get/to/the/point/pilot01/HelloController.java
@@ -0,0 +1,12 @@
+package get.to.the.point.pilot01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
